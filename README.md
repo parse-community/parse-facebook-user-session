447c2d68dc917c5eb422f8304e86e281c54780c5
Bryan Klimt <klimt@parse.com>
2014-11-14 13:16:56 -0800

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,82 @@ parse-facebook-user-session
 ===========================
 
 A Cloud Code module to facilitate logging into an express website with Facebook.
+
+If you'd like to require users to be logged into Facebook to access pages
+on your site, it's easy using the `parseFacebookUserSession`
+middleware module. First set up the `parseExpressCookieSession`
+as described in
+<a href="https://parse.com/docs/hosting_guide#webapp-users">our express docs</a>.
+
+    var parseExpressHttpsRedirect = require('parse-express-https-redirect');
+    var parseExpressCookieSession = require('parse-express-cookie-session');
+    var parseFacebookUserSession = require('cloud/parse-facebook-user-session');
+    
+    // ... Configure the express app ...
+    
+    app.use(parseExpressHttpsRedirect());  // Require user to be on HTTPS.
+    app.use(express.bodyParser());
+    app.use(express.cookieParser('YOUR_SIGNING_SECRET'));
+    app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
+
+Then use the `parseFacebookUserSession` middleware on any page
+that you want to require Facebook login on. Whenever the page is visited, if
+the user has not logged into your app with Facebook, they will be redirected
+to Facebook's site to start an OAuth flow. Once they get a login token from
+Facebook, they will be redirected back to an endpoint on your site. In the
+example below, the endpoint is `/login`. The middleware module
+takes care of handling that endpoint for you, authenticating the user and
+redirecting them back to the original page they tried to visit. To enable this
+on every page, use the `app.use` express method.</p>
+
+    app.use(parseFacebookUserSession({
+      clientId: 'YOUR_FB_CLIENT_ID',
+      appSecret: 'YOUR_FB_APP_SECRET',
+      redirectUri: '/login',
+    }));
+
+If you'd like to only require Facebook Login on certain pages, you can include
+the middleware in your routing commands.
+
+    var fbLogin = parseFacebookUserSession({
+      clientId: 'YOUR_FB_CLIENT_ID',
+      appSecret: 'YOUR_FB_APP_SECRET',
+      redirectUri: '/login',
+    }));
+
+    // To handle the login redirect.
+    app.get('/login', fbLogin, function(req, res) {});
+
+    app.get('/stuff', fbLogin, function(req, res) {
+      // This page requires Facebook login.
+    });
+
+You can access the user on any page with `Parse.User.current`.
+
+    app.get('/', function(req, res) {
+      var user = Parse.User.current();
+    
+      res.render('hello', {
+        message: 'Congrats, you are logged in, ' + user.get('name') + '!'
+      });
+    });
+
+You should also provide an endpoint to let the user log out. This is as simple
+as calling `Parse.User.logOut()`.
+
+    app.get('/logout', function(req, res) {
+      Parse.User.logOut();
+      res.render('hello', { message: 'You are now logged out!' });
+    });
+
+As a side effect of using this module, you will see objects created in your
+app's data for the `ParseFacebookTokenRequest` class. You can
+safely ignore this class. It is used to keep track of the CSRF protection
+tokens and redirect URLs of users who are currently in the process of
+logging in.
+
+In order for the `ParseFacebookTokenRequest` to be created, you may need to
+enabled client class creation in your app's settings, if you've previously
+disabled it. Once the `ParseFacebookTokenRequest` class is created, you should
+go into the [class-level permissions](http://blog.parse.com/2014/07/07/parse-security-ii-class-hysteria/) for the class and disable all permissions. This cloud module uses
+master key access to access the tables.
---
+++ parse-facebook-user-session-1.0.0.js
@@ -0,0 +1,187 @@
+
+var moment = require('moment');
+var querystring = require('querystring');
+
+/**
+ * @name parseFacebookUserSession
+ * @class
+ *
+ * <p>A middleware module for logging in a Parse.User using Facebook in express.
+ * For more information, see <a href="/docs/hosting_guide#webapp-facebook">our
+ * hosting guide</a>.</p>
+ *
+ * <p>Params includes the following:<pre>
+ *   clientId    (required): The client id of the Facebook App.
+ *   appSecret   (required): The app secret of the Facebook App.
+ *   verbose     (optional): If true, output debug messages to console.log.
+ *   redirectUri (optional): The path on this server to use for handling the
+ *       redirect callback from Facebook. If this is omitted, it defaults to
+ *       /login.</pre></p>
+ */
+var parseFacebookUserSession = function(params) {
+  params = params || {};
+
+  if (!params.clientId || !params.appSecret) {
+    throw "You must specify a Facebook clientId and appSecret.";
+  }
+
+  /**
+   * Logs using console.log if verbose is passed in when configuring the
+   * middleware.
+   */
+  var maybeLog = function() {
+    if (params.verbose) {
+      console.log.apply(console, arguments);
+    }
+  };
+
+  var relativeRedirectUri = params.redirectUri || "/login";
+
+  /**
+   * Returns the absolute url of the redirect path for this request.
+   */
+  var getAbsoluteRedirectUri = function(req) {
+    return 'https://' + req.host + relativeRedirectUri;
+  };
+
+  /**
+   * Starts the Facebook login OAuth process.
+   */
+  var beginLogin = function(req, res) {
+    maybeLog("Starting Facebook login...");
+
+    Parse.Promise.as().then(function() {
+      // Make a request object. Its objectId will be our XSRF token.
+      maybeLog("Creating ParseFacebookTokenRequest...");
+      var url = 'https://' + req.host + req.path;
+      var request = new Parse.Object("ParseFacebookTokenRequest");
+      return request.save({
+        url: url,
+        ACL: new Parse.ACL()
+      });
+
+    }).then(function(request) {
+      maybeLog("Redirecting for Facebook OAuth.");
+
+      // Put the XSRF token into a cookie so that we can match it later.
+      res.cookie("requestId", request.id);
+
+      // Redirect the user to start the Facebook OAuth flow.
+      var url = 'https://www.facebook.com/dialog/oauth?';
+      url = url + querystring.stringify({
+        client_id: params.clientId,
+        redirect_uri: getAbsoluteRedirectUri(req),
+        state: request.id
+      });
+      res.redirect(302, url);
+
+    });
+  };
+
+  /**
+   * Handles the last stage of the Facebook login OAuth redirect.
+   */
+  var endLogin = function(req, res) {
+    maybeLog("Handling request callback for Facebook login...");
+
+    if (req.query.state !== req.cookies.requestId) {
+      maybeLog("Request failed XSRF validation.");
+      res.send(500, "Bad Request");
+      return;
+    }
+
+    var url = 'https://graph.facebook.com/oauth/access_token?';
+    url = url + querystring.stringify({
+      client_id: params.clientId,
+      redirect_uri: getAbsoluteRedirectUri(req),
+      client_secret: params.appSecret,
+      code: req.query.code
+    });
+
+    var accessToken = null;
+    var expires = null;
+    var facebookData = null;
+
+    Parse.Promise.as().then(function() {
+      maybeLog("Fetching access token...");
+      return Parse.Cloud.httpRequest({ url: url });
+
+    }).then(function(response) {
+      maybeLog("Fetching user data from Facebook...");
+
+      var data = querystring.parse(response.text);
+      accessToken = data.access_token;
+      expires = data.expires;
+
+      var url = 'https://graph.facebook.com/me?';
+      url = url + querystring.stringify({
+        access_token: accessToken
+      });
+      return Parse.Cloud.httpRequest({ url: url });
+
+    }).then(function(response) {
+      maybeLog("Logging into Parse with Facebook token...");
+
+      facebookData = response.data;
+      var expiration = moment().add('seconds', expires).format(
+          "YYYY-MM-DDTHH:mm:ss.SSS\\Z");
+      
+      return Parse.FacebookUtils.logIn({
+        id: response.data.id,
+        access_token: accessToken,
+        expiration_date: expiration
+      });
+
+    }).then(function(response) {
+      maybeLog("Becoming Parse user...");
+      return Parse.User.become(response.sessionToken);
+
+    }).then(function(user) {
+      maybeLog("Saving Facebook data for user...");
+      user.set("name", facebookData.name);
+      return user.save();
+
+    }).then(function(user) {
+      maybeLog("Fetching ParseFacebookTokenRequest for " +
+               req.query.state + "...");
+      var request = new Parse.Object("ParseFacebookTokenRequest");
+      request.id = req.query.state;
+      return request.fetch({ useMasterKey: true });
+
+    }).then(function(request) {
+      maybeLog("Deleting used ParseFacebookTokenRequest...");
+      // Let's delete this request so that no one can reuse the token.
+      var url = request.get("url");
+      return request.destroy({ useMasterKey: true }).then(function() {
+        return url;
+      });
+
+    }).then(function(url) {
+      maybeLog("Success!");
+      res.redirect(302, url);
+    }, function(error) {
+      maybeLog("Failed! " + JSON.stringify(error));
+      res.send(500, error);
+    });
+  };
+
+  /**
+   * The actual middleware method.
+   */
+  return function(req, res, next) {
+    // If the user is already logged in, there's nothing to do.
+    if (Parse.User.current()) {
+      return next();
+    }
+
+    // If this is the Facebook login redirect URL, then handle the code.
+    var absoluteRedirectUri = 'https://' + req.host + relativeRedirectUri;
+    if (req.path === relativeRedirectUri) {
+      endLogin(req, res);
+    } else {
+      beginLogin(req, res);
+    }
+  };
+};
+
+module.exports = parseFacebookUserSession;
